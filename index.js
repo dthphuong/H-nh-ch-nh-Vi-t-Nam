c2fb1babe83dc78ed9c22d67b26894dd494cb5c0
Duong Tran Ha Phuong <phuongduong@fpo.vn>
2021-07-11 15:24:46 +0700

    fix duplicate data

--- index.js
+++ index.js
@@ -6,35 +6,40 @@ const _ = require('underscore');
 
 let provinces = [], districts = [], wards = [];
 
+console.log(`Start running . . . `);
 fs.createReadStream('./data/Danh sách cấp tỉnh kèm theo quận huyện, phường xã ___11_07_2021.csv')
     .pipe(parse({ delimiter: ',' }))
         .on('data', function (row) {
             if (row[1] != 'Mã TP') {
                 // ['Tỉnh Thành Phố,Mã TP,Quận Huyện,Mã QH,Phường Xã,Mã PX,Cấp,Tên Tiếng Anh']
                 // ['Thành phố Hà Nội,01,Quận Ba Đình,001,Phường Phúc Xá,00001,Phường,']
-                console.log(row);
+                // console.log(row);
 
                 //do something with row
-                provinces.push({
-                    "name": utils.extractName(row[0]).name,
-                    "slug": utils.getSlug(utils.extractName(row[0]).name),
-                    "type": utils.getSlug(utils.extractName(row[0]).type),
-                    "name_with_type": row[0],
-                    "code": row[1],
-                    "isDeleted": false
-                })
+                if (_.isUndefined(_.find(provinces, (item) => { return item.code == row[1] }))) {
+                    provinces.push({
+                        "name": utils.extractName(row[0]).name,
+                        "slug": utils.getSlug(utils.extractName(row[0]).name),
+                        "type": utils.getSlug(utils.extractName(row[0]).type),
+                        "name_with_type": row[0],
+                        "code": row[1],
+                        "isDeleted": false
+                    })
+                }
 
-                districts.push({
-                    "name": utils.extractName(row[2]).name,
-                    "type": utils.getSlug(utils.extractName(row[2]).type),
-                    "slug": utils.getSlug(utils.extractName(row[2]).name),
-                    "name_with_type": row[2],
-                    "path": utils.removeHCType(`${row[2]}, ${row[0]}`),
-                    "path_with_type": `${row[2]}, ${row[0]}`,
-                    "code": row[3],
-                    "parent_code": row[1],
-                    "isDeleted": false
-                })
+                if (_.isUndefined(_.find(districts, (item) => { return item.code == row[3] }))) {
+                    districts.push({
+                        "name": utils.extractName(row[2]).name,
+                        "type": utils.getSlug(utils.extractName(row[2]).type),
+                        "slug": utils.getSlug(utils.extractName(row[2]).name),
+                        "name_with_type": row[2],
+                        "path": utils.removeHCType(`${row[2]}, ${row[0]}`),
+                        "path_with_type": `${row[2]}, ${row[0]}`,
+                        "code": row[3],
+                        "parent_code": row[1],
+                        "isDeleted": false
+                    })
+                }
 
                 if (row[4].trim() != '') {
                     wards.push({
